d237dda6dd0bae55ea609f72a1f2d834e86a9c64
andreyliporage <andreyliporage@gmail.com>
2023-08-19 10:21:54 -0300

    criacao projeto

---
+++ src/main/java/com/pagpay/PagpaydesafioApplication.java
@@ -0,0 +1,13 @@
+package com.pagpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagpaydesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagpaydesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pagpay/PagpaydesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagpaydesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
